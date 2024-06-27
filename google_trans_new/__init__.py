63fb2ccf90ff5413698cbcbe66669e19e175c935
TrueSaiyan <ufperth@protonmail.com>
2024-06-28 02:50:52 +0800

    Update __init__.py

--- google_trans_new/__init__.py
+++ google_trans_new/__init__.py
@@ -2,7 +2,7 @@
 # author LuShan
 # version : 1.2.0
 __all__ = 'google_translator'
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 from .google_trans_new import google_translator
 from .constant import DEFAULT_SERVICE_URLS, LANGUAGES
